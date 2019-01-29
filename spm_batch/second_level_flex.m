975f5ad92293cae2d7694d34835e8e857fdbecbe
Michał Szczepanik <m.szczepanik@nencki.gov.pl>
2019-01-29 18:24:40 +0100

    Revert "Add option to change folders in 2nd lvl flex"
    This reverts commit 117985eddbc1de28e2de04c970fd780ca2d85b7a.

--- spm_batch/second_level_flex.m
+++ spm_batch/second_level_flex.m
@@ -1,17 +1,6 @@
 config = ini2struct('../movrest_config.ini');
 WORK_DIR = config.default.work_dir;
 
-% quick but dirty way to manually change subfolders
-if exist('OVERRIDE', 'var')
-    dir_1st_lvl = OVERRIDE.dir_1st;
-    dir_2nd_lvl = OVERRIDE.dir_2nd;
-    disp(['Reading from ' fullfile(WORK_DIR, dir_1st_lvl)]);
-    disp(['Using folder ' fullfile(WORK_DIR, dir_2nd_lvl)]);
-else
-    dir_1st_lvl = 'models';
-    dir_2nd_lvl = 'flex_factorial';
-end
-
 subjects = {'03'; '04'; '05'; '06'; '07'; '08'; '09'; '10'; '11'; '12'; '14'; '16'; '17'; '18'; '19'; '20'; '21'; '22'; '23'; '25'; '26'; '27'};
 
 mask_used = cellstr('../tpm_grey_20.nii,1');
@@ -33,7 +22,7 @@ for cn = 1:n_conditions
         
         sub_folder = sprintf('sub-%s', subjects{sn});
         con_fname = sprintf('con_%04d.nii', cn);
-        con_path = fullfile(WORK_DIR, dir_1st_lvl, sub_folder, con_fname);
+        con_path = fullfile(WORK_DIR, 'models', sub_folder, con_fname);
         
         factor_matrix(row_number, :) = [row_number cn sn 1];  % [1 cn sn 1]
         scans{row_number} = con_path;
@@ -49,7 +38,7 @@ inputs = cell(5, nrun);
 for crun = 1:nrun
     % Factorial design specification: Directory - cfg_files
     inputs{1, crun} = cellstr(fullfile(...
-        WORK_DIR, 'models2', dir_2nd_lvl));
+        WORK_DIR, 'models2', 'flex_factorial'));
     
     % Factorial design specification: Scans - cfg_files
     inputs{2, crun} = scans;
